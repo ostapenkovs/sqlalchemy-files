b8a1f61a5398bf1e4de6939970b70d4058020b1c
Vasiliy Ostapenko <ostapenkovs@pm.me>
2023-08-27 21:21:39 -0400

    working demo

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# @TODO: Explanation of web application
+## Dockerized Flask web application with PostgreSQL to demo binary file upload and download.
+
+### A user is able to upload as many files as they wish, see a list of all files in the database, and download a chosen file.
+
+*app.py*
+- Contains all back-end code and logic.
+
+*templates*
+- Contains front-end HTML / CSS / Jinja.
+
+*Docker-related*
+- Dockerfile and docker-compose.yml are the recipies specifying what our Docker container will have and do.
+
+*Getting started*
+1. "make build" builds the Docker image;
+2. "make run" runs the web application and database;
+3. "make clean" removes containers and locally-stored files related to containers.
--- app.py
+++ app.py
@@ -1,12 +1,78 @@
-from flask import (
-    Flask
+# Necessary imports
+import os
+import io
+from werkzeug.utils import secure_filename
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from sqlalchemy import (
+    Column,
+    Integer,
+    DateTime,
+    String,
+    LargeBinary
 )
 
-app = Flask(__name__)
+from flask import (
+    Flask,
+    request,
+    render_template,
+    abort,
+    send_file,
+    make_response
+)
 
+# Initializing Flask app and SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
+db = SQLAlchemy(app)
+
+# Creating the SQLAlchemy model for the file table
+class File(db.Model):
+    __tablename__ = 'file'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime(timezone=True), server_default=func.now())
+    name = Column(String(100), nullable=False)
+    data = Column(LargeBinary, nullable=False)
+
+# Creating the table in the database
+with app.app_context():
+    db.create_all()
+
+# Registering the internal server error (500 code) handler
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.jinja'), 500
+
+# Index route allows user to upload new files as well as to see and download
+# any files already present in the database
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'No'
+    files = None
 
+    try:
+        if request.method == 'POST':
+            for file in request.files.getlist('files'):
+                obj = File(name=secure_filename(file.filename), data=file.read())
+                db.session.add(obj)
+            
+            db.session.commit()
+
+        files = [(x.id, x.name, x.date) for x in db.session.query(File).all()]
+
+    except Exception as e:
+        print(e)
+        abort(500)
+    
+    return render_template('index.jinja', files=files)
+
+# Download route handles file downloads
+@app.route('/download/<id>', methods=['GET'])
+def download(id):
+    obj = db.session.query(File).filter(File.id == id).first()
+    return send_file(io.BytesIO(obj.data), download_name=obj.name, as_attachment=True)
+
+# Running the Flask app on all hosts (important for Docker)
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
